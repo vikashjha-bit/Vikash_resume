95994c18d95fc48931743c9df4534f1de34f8e1d
vikash jha <jhavikashkumar716@gmail.com>
2025-08-20 20:13:53 +0530

    Your message here

--- src/componenets/About/About.jsx
+++ src/componenets/About/About.jsx
@@ -73,7 +73,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={vite.png}
+              src={profileImage}
               alt="Vikash Jha"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
